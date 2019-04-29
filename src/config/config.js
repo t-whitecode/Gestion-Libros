6692379ce68aae76401da2db4adac8e2c3aea2b1
juancarozo <jpiceruso@gmail.com>
2019-04-29 10:04:11 -0500

    error en config

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,16 @@
 process.env.PORT = process.env.PORT || 3000;
 
-let urlDB
-urlDB = 'mongodb+srv://Juancarozo:<juancarozo>@gestionlibros-zhhhg.mongodb.net/test?retryWrites=true'
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'local';
+
+let urlDB
+if (process.env.NODE_ENV === 'local'){
+	urlDB = 'mongodb://localhost:27017/test';
+}
+
+else {
+	urlDB = 'mongodb+srv://Juancarozo:<juancarozo>@gestionlibros-zhhhg.mongodb.net/test?retryWrites=true'
+}
 
 process.env.SENDGRID_API_KEY='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'
 
