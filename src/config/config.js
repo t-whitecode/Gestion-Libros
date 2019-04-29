0b31e9b9e548a0a6f99d42b121bd902aac5a4249
juancarozo <jpiceruso@gmail.com>
2019-04-28 22:52:34 -0500

    Cambiomongoose

--- src/config/config.js
+++ src/config/config.js
@@ -1,15 +1,8 @@
 process.env.PORT = process.env.PORT || 3000;
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'local';
-
 let urlDB
-if (process.env.NODE_ENV === 'local'){
-	urlDB = 'mongodb://localhost:27017/test';
-}
+urlDB = 'mongodb+srv://Juancarozo:<juancarozo>@gestionlibros-zhhhg.mongodb.net/test?retryWrites=true'
 
-else {
-	urlDB = 'mongodb+srv://Sprint02:t-whitecode@sprint02-5la4f.mongodb.net/test?retryWrites=true'
-}
 
 process.env.SENDGRID_API_KEY='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'
 
